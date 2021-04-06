bc603668ec795bd0acee922286357208077a96f3
Dairo Tenorio <dairotenorio@gmail.com>
2021-04-05 20:07:10 -0500

    Adding multiplication and Division Adding rebased changes

--- bootcampcalculator/src/main/java/com/automation/bootcamp/App.java
+++ bootcampcalculator/src/main/java/com/automation/bootcamp/App.java
@@ -9,10 +9,16 @@ public class App
     public static void main( String[] args )
     {
         App myApp = new App();
+
         double mySum = myApp.sum(10.5, 9.5);
         double mySub = myApp.sub(10.5, 9.5);
         System.out.println("Sum is: " + mySum);
         System.out.println("Substraction is: " + mySub);
+
+        double myMult = myApp.mult(5, 5);
+        double myDiv = myApp.div(5, 5);
+        System.out.println("Multiplication is: " + myMult);
+        System.out.println("Division is: " + myDiv);
     }
 
     public double sum(double firstNum, double secondNum){
@@ -24,6 +30,22 @@ public class App
     public double sub(double firstNum, double secondNum){
         double result;
         result = firstNum - secondNum;
+        return result;
+    }
+
+    public double mult(double firstNum, double secondNum){
+        double result;
+        result = firstNum * secondNum;
+        return result;
+    }
+
+    public double div(double firstNum, double secondNum){
+        double result;
+        if (secondNum == 0.0){ 
+            result = 0.0;
+        }else {
+            result = firstNum / secondNum;
+        }
         return result;
     }
 }
