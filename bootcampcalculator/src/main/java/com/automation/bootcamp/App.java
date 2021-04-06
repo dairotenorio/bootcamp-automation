9cad27c30340d3059052e236073faf727d9ac870
Dairo Tenorio <dairotenorio@gmail.com>
2021-04-06 13:18:43 -0500

    new commit for remake changes

--- bootcampcalculator/src/main/java/com/automation/bootcamp/App.java
+++ bootcampcalculator/src/main/java/com/automation/bootcamp/App.java
@@ -8,6 +8,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Substraction is");
+        App myApp = new App();
+        double mySum = myApp.sum(10.5, 9.5);
+        double mySub = myApp.sub(10.5, 9.5);
+        System.out.println("Sum is: " + mySum);
+        System.out.println("Substraction is: " + mySub);
+    }
+
+    public double sum(double firstNum, double secondNum){
+        double result;
+        result = firstNum + secondNum;
+        return result;
+    }
+
+    public double sub(double firstNum, double secondNum){
+        double result;
+        result = firstNum - secondNum;
+        return result;
     }
 }
